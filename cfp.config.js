91251077cb7be2a73d3b23a6c1ee9fe9da719c04
szabolcsit <szabolcsit@hu.ibm.com>
2019-03-06 22:16:56 -0600

    update cfp config & dev startup

--- cfp.config.js
+++ cfp.config.js
@@ -11,21 +11,15 @@ module.exports = {
 		},
 		'stage_2': {
 			label: 'Shortlisting Round',
-			include_votes_top: 6
+			include_votes_top: 60
 		}
 	},
 	voting_ui: {
 		stage_1: [
-			{ label: '1', value: 1 },
-			{ label: '2', value: 2 },
-			{ label: '3', value: 3 },
-			{ label: '4', value: 4 },
-			{ label: '5', value: 5 },
-			{ label: '6', value: 6 },
-			{ label: '7', value: 7 },
-			{ label: '8', value: 8 },
-			{ label: '9', value: 9 },
-			{ label: '10', value: 10 },
+			{ label: 'nope', 		value: 0 },
+			{ label: 'seen better',	value: 2 },
+			{ label: 'maybe', 		value: 4 },
+			{ label: 'absolutely', 	value: 8 },
 		],
 		stage_2: [
 			{ label: 'meh', value: 1 },
