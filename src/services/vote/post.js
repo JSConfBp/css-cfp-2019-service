17180e5275290b4fe2324bbeeda1ce7c4de80c30
Szabolcs Szabolcsi-Toth <szabolcsit@hu.ibm.com>
2019-04-05 20:40:30 +0200

    chore: remove log

--- src/services/vote/post.js
+++ src/services/vote/post.js
@@ -2,6 +2,11 @@ const store = require('../../store')
 const getLoginName = require('../../auth/loginName')
 const { getUserStagedVotesKey } = store.keys
 
+
+const wait = async (time) => (new Promise((resolve) => {
+	setTimeout(resolve, time)
+}))
+
 module.exports = async function (request) {
 
 	const { payload: vote } = request
@@ -10,7 +15,7 @@ module.exports = async function (request) {
 
 	const key = getUserStagedVotesKey(login, stage)
 
-// todo check if talk id already exist in votes?
+	// todo check if talk id already exist in votes?
 
 	await store.rpush(key, JSON.stringify(vote))
 
